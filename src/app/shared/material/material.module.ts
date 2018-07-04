221b1b5c12ec827d769c36809e93ba715f73faf2
Fabio Iwakoshi <fabio.iwakoshi@gmail.com>
2018-07-03 21:18:00 -0300

    feat: add material design

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatInputModule],
+  exports: [MatButtonModule, MatCardModule, MatIconModule, MatInputModule]
+})
+export class MaterialModule {}
